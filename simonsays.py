a7a7868dbeea4928a2942d8a6552dba3a14e48a3
Estevao Fonseca <estevaopfon@gmail.com>
2019-10-21 11:47:34 -0300

    v.1.0 complete

--- simonsays.py
+++ simonsays.py
@@ -1,22 +1,20 @@
-from tkinter import *
+import tkinter
 import random
+import time
 
-root = Tk()
+root = tkinter.Tk()
 
-w = Canvas(root, width=400, height=400)
+w = tkinter.Canvas(root, width=400, height=400)
 w.pack()
+colors_clicked = []
 
 def default_colors():
-    w.create_rectangle(0, 0, 200, 200, fill="red", tag='red')
-    w.create_rectangle(200, 0, 400, 200, fill="blue", tag='blue')
+    w.create_rectangle(0, 0, 200, 200, fill="green", tag='green')
+    w.create_rectangle(200, 0, 400, 200, fill="red", tag='red')
     w.create_rectangle(0, 200, 200, 400, fill="yellow", tag='yellow')
-    w.create_rectangle(200, 200, 400, 400, fill="green", tag='green')
+    w.create_rectangle(200, 200, 400, 400, fill="blue", tag='blue')
 
 default_colors()
-
-colors = ['red', 'blue', 'yellow', 'green']
-positions = {'red': (0, 0, 200, 200), 'blue': (200, 0, 400, 200), 'yellow': (0, 200, 200, 400), 'green': (200, 200, 400, 400)}
-
 
 def showxy(event):
     '''
@@ -25,26 +23,93 @@ def showxy(event):
     '''
     x1 = 0
     y1 = 0
-    xy = 'rectangle x=%s  y=%s' % (event.x-x1, event.y-y1)
-    root.title(xy) 
+    x = event.x-x1
+    y = event.y-y1
+    message = 'rectangle x=%s  y=%s' % (x, y)
+    if x < 200 and y <200: 
+        message = 'green'
+    elif x > 200 and y <200: 
+        message = 'red'
+    elif x < 200 and y > 200: 
+        message = 'yellow'
+    else:
+        message = 'blue'
+    colors_clicked.append(message)
+    print(colors_clicked)
+    light_rect(message)
+    w.update()
+    time.sleep(0.3)
+    w.update()
+    default_colors()
+
+    root.title(message) 
 
 w.tag_bind('red', '<Button-1>', showxy)
 w.tag_bind('green', '<Button-1>', showxy)
 w.tag_bind('blue', '<Button-1>', showxy)
 w.tag_bind('yellow', '<Button-1>', showxy)
 
+colors = ['red', 'blue', 'yellow', 'green']
+positions = {'green': (0, 0, 200, 200), 'red': (200, 0, 400, 200), 'yellow': (0, 200, 200, 400), 'blue': (200, 200, 400, 400)}
 light_colors = {'yellow': '#FBFF9E', 'red': '#FF7959', 'blue': '#95B4FF', 'green': '#95FFA0'}
 seq = []
-seq = ['blue', 'yellow','red','blue']
+delta_time = 1000 
+
+def light_rect(color):
+    position_tuple = positions[color]
+    light_color = light_colors[color]
+    w.create_rectangle(*position_tuple, fill=light_color)
+
+def paint_color():
+    for color in seq:
+        position_tuple = positions[color]
+        light_color = light_colors[color]
+        w.create_rectangle(*position_tuple, fill=light_color)
+        time.sleep(0.5)
+        w.update()
+        time.sleep(1)
+        default_colors()
+        w.update()
+
 
 def task():
     default_colors()
-    root.after(1000, task)  # reschedule event in 2 seconds
     color = random.choice(colors)
-    position_tuple = positions[color]
-    color = light_colors[color]
-    w.create_rectangle(*position_tuple, fill=color)
+    seq.append(color)
+    paint_color()
+    global colors_clicked
+    colors_clicked = []
+    root.after(1000, clean_colors)
+
+def clean_colors():
+    default_colors()
+    global delta_time
+    delta_time += 1000
+    root.after(delta_time, check)
+
+def restart():
+    global delta_time
+    delta_time = 1000
+    global seq
+    seq = []
+
+def check():
+    if seq == colors_clicked:
+        print("Acertou!")
+    else:
+        print("errou")
+        global colors
+        for i in range(3):
+            for color in colors:
+                light_rect(color)
+            w.update()
+            time.sleep(0.5)
+            default_colors()
+            w.update()
+            time.sleep(0.5)
+        restart()
+    root.after(1000, task)
 
 
 root.after(1000, task)
-mainloop()
+tkinter.mainloop()
